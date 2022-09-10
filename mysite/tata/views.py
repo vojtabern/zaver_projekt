f962dea4ebf6dc6e4a33011196263693ca2377cb
vojtabern <vojtabern@gmail.com>
2022-09-10 16:05:57 +0200

    still not as i want to

--- mysite/tata/views.py
+++ mysite/tata/views.py
@@ -22,15 +22,6 @@ def index(request):
 
 
 def vzdelani(request):
-    #y = Vzdelani.objects.all()
-    #grad = all(x.OPTIONS for x in y)
-    typ = Vzdelani.typ
-    num_vzde = Vzdelani.objects.all().count()
-    vzdelani_vse = Vzdelani.objects.all()
-    vse = []
-    for i in range(0, num_vzde):
-        vse.append(i)
-
     vzdelani = Vzdelani.objects.filter(typ='základní vzdělání').values()
     vzdelani1 = Vzdelani.objects.filter(typ='Psychoterapeutický výcvik').values()
     vzdelani2 = Vzdelani.objects.filter(typ='Doplňkové vzdělání').values()
@@ -39,14 +30,12 @@ def vzdelani(request):
     vzdelani5 = Vzdelani.objects.filter(typ='Výzkum a teoretické práce').values()
 
     context = {
-        'daty':vzdelani_vse,
         'vzdelani0':vzdelani,
         'vzdelani1':vzdelani1,
         'vzdelani2': vzdelani2,
         'vzdelani3': vzdelani3,
         'vzdelani4': vzdelani4,
         'vzdelani5':vzdelani5,
-        'vse':vse,
     }
     return render(request, 'vzdelani.html', context=context)
 
