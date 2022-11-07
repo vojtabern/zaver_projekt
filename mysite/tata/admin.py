11bcb5438223851661df6a33f51141996c6cf9ee
vojtabern <vojtabern@gmail.com>
2022-11-07 17:25:21 +0100

    working on model

--- mysite/tata/admin.py
+++ mysite/tata/admin.py
@@ -5,5 +5,7 @@ from . models import *
 admin.site.register(Psycholog)
 admin.site.register(basicInfo)
 admin.site.register(Vzdelani)
+admin.site.register(Client)
+admin.site.register(User)
 
 # Register your models here.
--- mysite/tata/models.py
+++ mysite/tata/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
+
+
+
+
+class User(models.Model):
+    description = "User"
+    id_user = models.IntegerField(default=1, primary_key=True)
+
+    def __str__(self):
+        return self.description
+
+
 class Psycholog(models.Model):
     id_psycholog = models.IntegerField(default=1, primary_key=True)
     username = models.CharField(max_length=45)
     password = models.CharField(max_length=50)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+
 
     class Meta:
         ordering = ["username"]
@@ -11,13 +25,48 @@ class Psycholog(models.Model):
     def __str__(self):
         return self.username
 
+class Vzdelani(models.Model):
+    OPTIONS = (
+        ('základní vzdělání', 'základní vzdělání'),
+        ('Psychoterapeutický výcvik', 'Psychoterapeutický výcvik'),
+        ('Doplňkové vzdělání', 'Doplňkové vzdělání'),
+        ('Diagnostika', 'Diagnostika'),
+        ('Pracovní zkušenosti', 'Pracovní zkušenosti'),
+        ('Výzkum a teoretické práce', 'Výzkum a teoretické práce'),
+    )
+    nazev = models.CharField(max_length=200)
+    typ = models.CharField(max_length=45, choices=OPTIONS, default='základní vzdělání')
+    psycholog = models.ForeignKey(Psycholog, on_delete=models.CASCADE, default=1)
+
+    def get_options(self):
+        vrat = (zakl, psycho, dopln, dia, prace, vyzkum) = self.OPTIONS
+        ifo = (vrat[0][0], vrat[1][0], vrat[2][0], vrat[3][0], vrat[4][0], vrat[5][0])
+        return ifo
+
+    class Meta:
+        ordering = ["typ"]
+
+    def __str__(self):
+        return self.nazev
+
+class Client(models.Model):
+    id_client = models.IntegerField(default=1, primary_key=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["id_client"]
+
+    def __str__(self):
+        return self.id_client
+
+
 class basicInfo(models.Model):
     description = "Zakl. info"
     id_basicInfo = models.IntegerField(default=1, primary_key=True)
     provozovna = models.CharField(default='Hradecká 16, Opava',max_length=200)
     telefon = models.CharField(default='+420 737 881 112',max_length=18)
     email = models.EmailField(default='jbernard@hotmail.cz')
-    fk_psycholog = models.ForeignKey(Psycholog,default=1, on_delete=models.CASCADE)
+    psycholog_id = models.OneToOneField(Psycholog, on_delete=models.CASCADE ,default=1)
 
     class Meta:
         ordering = ["email"]
@@ -31,28 +80,7 @@ class basicInfo(models.Model):
         return default
 
 
-class Vzdelani(models.Model):
-    OPTIONS = (
-        ('základní vzdělání','základní vzdělání'),
-        ('Psychoterapeutický výcvik','Psychoterapeutický výcvik'),
-        ('Doplňkové vzdělání', 'Doplňkové vzdělání'),
-        ('Diagnostika', 'Diagnostika'),
-        ('Pracovní zkušenosti','Pracovní zkušenosti'),
-        ('Výzkum a teoretické práce','Výzkum a teoretické práce'),
-    )
-    nazev = models.CharField( max_length=200)
-    typ = models.CharField(max_length=45, choices=OPTIONS, default='základní vzdělání')
 
-    def get_options(self):
-        vrat = (zakl, psycho, dopln, dia, prace, vyzkum) = self.OPTIONS
-        ifo = (vrat[0][0], vrat[1][0], vrat[2][0], vrat[3][0], vrat[4][0], vrat[5][0])
-        return ifo
-
-    class Meta:
-        ordering = ["typ"]
-
-    def __str__(self):
-        return self.nazev
 
 # tady class
 # Create your models here.
